16809b44e91b2dd74315dc1080e13a465cc54bec
Yasir <yasir.ali@email.ucr.edu>
2017-04-30 10:44:10 -0700

    Tested Fx, Chrome, Edge, all working same ah * Also simplified out the non redux proxy stuff * Need to fix the disconnect issue while keeping in mind Edge is not firing disconnect on tab close

--- src/webext/background/index.js
+++ src/webext/background/index.js
@@ -5,22 +5,11 @@ import renderProxiedElement, { Server as ReduxServer } from '../common/comm/redu
 
 import * as reducers from '../flows'
 
-import { wait } from '../common/all'
-
 import BackgroundElement from './BackgroundElement'
 
-const gPortsComm = new PortsServer(exports, handlePortHandshake); // eslint-disable-line no-unused-vars
+const gPortsComm = new PortsServer(exports ); // eslint-disable-line no-unused-vars
 export const callInPort = callInTemplate.bind(null, gPortsComm, null);
 export const callIn = (...args) => new Promise(resolve => exports['callIn' + args.shift()](...args, val=>resolve(val))); // must pass undefined for aArg if one not provided, due to my use of spread here. had to do this in case first arg is aMessageManagerOrTabId
-
-async function handlePortHandshake(portname) {
-    await wait(5000);
-    callInPort(portname, 'showAlert', 'hand shaken');
-}
-
-export function logit(what) {
-    console.log('logit:', what);
-}
 
 export const gReduxServer = new ReduxServer(reducers);
 
