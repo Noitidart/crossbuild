52da68b96d57aef2c2ba4392a724ea46a39703ba
Yasir <yasir.ali@email.ucr.edu>
2017-05-16 11:23:12 -0700

    Showing how to use CSS variables for theme

--- src/web/index.js
+++ src/web/index.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+import './theme-a.css'
 import './index.css'
 
 console.error('ENTER');
 
 class App extends Component {
+    state = {
+        theme: undefined
+    }
+    useThemeB = () => this.setState(()=>({theme:'./theme-b'}));
+    useThemeDefault = () => this.setState(()=>({theme:undefined}));
     render() {
+        let { theme } = this.state;
         let arr = [<span>1</span>, <span>2</span>, <span>3</span>];
         return (
             <div>
+                {theme && <link href={theme+'.css'} rel="stylesheet" /> }
                 {arr}
+                <button onClick={this.useThemeB}>Use Theme B</button>
+                <button onClick={this.useThemeDefault}>Use Theme Defeault</button>
             </div>
         );
     }
